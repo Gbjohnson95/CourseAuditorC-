e36150b200e69940758088279cc7c9d66356acfe
Grant Johnson <gbjohnson@me.com>
2016-01-26 10:25:21 -0700

    Initial Commit

---
+++ CourseAuditor/CourseAuditor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CourseAuditor
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			CourseDocument test = new CourseDocument ("C:\\Users\\gbjohnson\\Desktop\\test.html");
+		}
+	}
+}
---
+++ CourseAuditor/CourseDocument.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using HtmlAgilityPack;
+using System.Xml.XPath;
+
+namespace CourseAuditor
+{
+	public class CourseDocument
+	{
+		private HtmlAgilityPack.HtmlDocument htmlDoc;
+		
+		public CourseDocument (String filePath)
+		{
+			htmlDoc = new HtmlAgilityPack.HtmlDocument();
+			htmlDoc.Load (filePath);
+
+			//* Test cases
+			Console.WriteLine ("Spans: " + CountQuery("//span"));
+			Console.WriteLine ("Regex: " + CountRegEx ("<span>"));
+			Console.WriteLine ("Title: " + getHtmlTitle());
+			Console.ReadLine(); // Gheto wait function
+			//*/
+		}
+
+		public int CountQuery (String query) {
+			return htmlDoc.DocumentNode.SelectNodes(query).Count;
+		}
+
+		public int CountRegEx (String pattern) {
+			return Regex.Matches(htmlDoc.DocumentNode.OuterHtml, pattern).Count;
+		}
+
+		public String getHtmlTitle () {
+			String title, rawTitle = htmlDoc.DocumentNode.SelectSingleNode ("//title").InnerText;
+			if (rawTitle != null && rawTitle != "") {
+				title = htmlDoc.DocumentNode.SelectSingleNode ("//title").InnerText;
+			} else {
+				title = "ERROR: NO TITLE FOUND";
+			}
+			return title;
+		}
+
+	}
+
+
+}
+
