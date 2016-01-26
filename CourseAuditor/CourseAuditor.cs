7355751dc0733b78e9abada18fe370194ae57732
Grant Johnson <gbjohnson@me.com>
2016-01-26 10:59:08 -0700

    More functions moved over

--- CourseAuditor/CourseAuditor.cs
+++ CourseAuditor/CourseAuditor.cs
@@ -6,7 +6,12 @@ namespace CourseAuditor
 	{
 		public static void Main (string[] args)
 		{
-			CourseDocument test = new CourseDocument ("C:\\Users\\gbjohnson\\Desktop\\test.html");
+			CourseDocument test = new CourseDocument ();
+			test.loadDoc ("C:\\Users\\gbjohnson\\Desktop\\test.html", "Test Document", "16094", "123456");
+		}
+
+		public void parseManifestAndRun( String resultname, String path ) {
+
 		}
 	}
 }
--- CourseAuditor/CourseDocument.cs
+++ CourseAuditor/CourseDocument.cs
@@ -8,38 +8,88 @@ namespace CourseAuditor
 	public class CourseDocument
 	{
 		private HtmlAgilityPack.HtmlDocument htmlDoc;
+		private String DocName, OrgUnitID, DocID;
 		
-		public CourseDocument (String filePath)
+		public CourseDocument ()
 		{
+		}
+
+		public void loadDoc (String filepath, String docname, String orgunitid, String docid) {
+			// Load file into parser
 			htmlDoc = new HtmlAgilityPack.HtmlDocument();
-			htmlDoc.Load (filePath);
+			htmlDoc.Load (filepath);
+
+			// Set variables
+			DocName = docname;
+			OrgUnitID = orgunitid;
+			DocID = docid;
 
 			//* Test cases
-			Console.WriteLine ("Spans: " + CountQuery("//span"));
-			Console.WriteLine ("Regex: " + CountRegEx ("<span>"));
-			Console.WriteLine ("Title: " + getHtmlTitle());
+			Console.WriteLine ("Spans:  " + CountQuery("//span"));
+			Console.WriteLine ("Regex:  " + CountRegEx ("<span>"));
+			Console.WriteLine ("Title:  " + getHtmlTitle());
+			Console.WriteLine ("Header: " + checkHeaders());
+			Console.WriteLine ("Link:   " + generateD2lLink ());
 			Console.ReadLine(); // Gheto wait function
 			//*/
 		}
 
+		// Counts an XPath query
 		public int CountQuery (String query) {
 			return htmlDoc.DocumentNode.SelectNodes(query).Count;
 		}
 
+		// Counts occureneces of a Regular Expression
 		public int CountRegEx (String pattern) {
 			return Regex.Matches(htmlDoc.DocumentNode.OuterHtml, pattern).Count;
 		}
 
+		// Gets the HTML title
 		public String getHtmlTitle () {
 			String title, rawTitle = htmlDoc.DocumentNode.SelectSingleNode ("//title").InnerText;
 			if (rawTitle != null && rawTitle != "") {
-				title = htmlDoc.DocumentNode.SelectSingleNode ("//title").InnerText;
+				title = rawTitle;
 			} else {
 				title = "ERROR: NO TITLE FOUND";
 			}
 			return title;
 		}
 
+		// Checks the headers for ADA compliance
+		public String checkHeaders () {
+			String headers = "";
+			if (htmlDoc.DocumentNode.SelectSingleNode("//h1") != null) {
+				headers += "1";
+			}
+			if (htmlDoc.DocumentNode.SelectSingleNode("//h2") != null) {
+				headers += "2";
+			}
+			if (htmlDoc.DocumentNode.SelectSingleNode("//h3") != null) {
+				headers += "3";
+			}
+			if (htmlDoc.DocumentNode.SelectSingleNode("//h4") != null) {
+				headers += "4";
+			}
+			if (htmlDoc.DocumentNode.SelectSingleNode("//h5") != null) {
+				headers += "5";
+			}
+			if (htmlDoc.DocumentNode.SelectSingleNode("//h6") != null) {
+				headers += "6";
+			}
+			if (headers == "") {
+				headers = "No Headers";
+			}
+			if ("123456".IndexOf (headers) == 0) {
+				return "Good: " + headers;
+			} else {
+				return "Bad: " + headers;
+			}
+		}
+
+		// Generates a working link to the document
+		public String generateD2lLink () {
+			return "https://byui.brightspace.com/d2l/le/content/" + OrgUnitID + "/viewContent/" + DocID + "/View";
+		}
 	}
 
 
