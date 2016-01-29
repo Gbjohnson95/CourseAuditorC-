290f32d6ea6132c543a9f207c7c81e63e31d7936
Grant Johnson <gbjohnson@me.com>
2016-01-29 13:43:59 -0700

    Formatted Code

--- CourseAuditor/CourseAuditor.cs
+++ CourseAuditor/CourseAuditor.cs
@@ -8,192 +8,224 @@ using CsQuery;
 
 namespace CourseAuditor
 {
-	class MainClass
-	{
-		private static XmlDocument config;
+    class MainClass
+    {
+        private static XmlDocument config;
 
-		// Feeds the config path into readConfigAndRun -------------------------------------------------------------------------------------------------
-		public static void Main (string[] args) {
-			for (int i = 0; i < args.Length; i++) {
-				readConfigAndRun (args[i]);
-			}
-			Console.Beep();
-		}
+        // Feeds the config path into readConfigAndRun -------------------------------------------------------------------------------------------------
+        public static void Main(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                readConfigAndRun(args[i]);
+            }
+            Console.Beep();
+        }
 
-		// Reads in Config, and grabs the target folder and target report file -------------------------------------------------------------------------
-		public static void readConfigAndRun(String configPath) {
-				config = new XmlDocument();
-				config.Load (configPath);
-				unZipAndRun(getTextNode (config.GetElementsByTagName ("containerFolder")[0]), getTextNode(config.GetElementsByTagName ("outFile") [0]));
+        // Reads in Config, and grabs the target folder and target report file -------------------------------------------------------------------------
+        public static void readConfigAndRun(String configPath)
+        {
+            config = new XmlDocument();
+            config.Load(configPath);
+            unZipAndRun(getTextNode(config.GetElementsByTagName("containerFolder")[0]), getTextNode(config.GetElementsByTagName("outFile")[0]));
 			
-		}
+        }
 
-		// Given the target folder, it will unzip any zips, and then send folders with imsmanifest.xml into manifest parser. ---------------------------
-		public static void unZipAndRun ( String topDir, String reportFile ) {
-			String[] zipsList = Directory.GetFiles (topDir, "*.zip");
-			String extractFolder;
-			startReportFile (reportFile);
-			PrintToFile (reportFile, getHeader ());
+        // Given the target folder, it will unzip any zips, and then send folders with imsmanifest.xml into manifest parser. ---------------------------
+        public static void unZipAndRun(String topDir, String reportFile)
+        {
+            String[] zipsList = Directory.GetFiles(topDir, "*.zip");
+            String extractFolder;
+            startReportFile(reportFile);
+            PrintToFile(reportFile, getHeader());
 
-			// Extract zips --------------------------------------------------------------------------------
-			if (zipsList.Length > 0) {
-				Console.WriteLine ("---------- EXTRACTING ZIPS           ----------\n");
-				for (int i = 0; i < zipsList.Length; i++) {
-					// Generate extact folder name
-					extractFolder = Path.Combine (topDir, zipsList [i].Replace (".zip", ""));
+            // Extract zips --------------------------------------------------------------------------------
+            if (zipsList.Length > 0)
+            {
+                Console.WriteLine("---------- EXTRACTING ZIPS           ----------\n");
+                for (int i = 0; i < zipsList.Length; i++)
+                {
+                    // Generate extact folder name
+                    extractFolder = Path.Combine(topDir, zipsList[i].Replace(".zip", ""));
 
-					// If that folder already exists, skip
-					if (!Directory.Exists (Path.Combine (topDir, extractFolder))) {
-						System.IO.Directory.CreateDirectory (extractFolder);
-						Console.WriteLine ("About to extract  " + new DirectoryInfo (zipsList [i]).Name);
-						ZipFile.ExtractToDirectory (Path.Combine (topDir, zipsList [i]), extractFolder);
-						Console.WriteLine ("\tExtracted " + new DirectoryInfo (zipsList [i]).Name + "\n");
-					}
-				}
-				Console.WriteLine ("---------- FINISHED EXTRACTING ZIPS  ----------\n\n");
-			}
+                    // If that folder already exists, skip
+                    if (!Directory.Exists(Path.Combine(topDir, extractFolder)))
+                    {
+                        System.IO.Directory.CreateDirectory(extractFolder);
+                        Console.WriteLine("About to extract  " + new DirectoryInfo(zipsList[i]).Name);
+                        ZipFile.ExtractToDirectory(Path.Combine(topDir, zipsList[i]), extractFolder);
+                        Console.WriteLine("\tExtracted " + new DirectoryInfo(zipsList[i]).Name + "\n");
+                    }
+                }
+                Console.WriteLine("---------- FINISHED EXTRACTING ZIPS  ----------\n\n");
+            }
 
-			// Run folders that contain a manifest  ---------------------------------------------------------
-			Console.WriteLine ("---------- AUDITING COURSES          ---------- ");
-			String[] folders = Directory.GetDirectories(topDir);
-			for (int h = 0; h < folders.Length; h++) {
-				if (File.Exists (Path.Combine (folders [h], "imsmanifest.xml"))) {
-					try {
-						Console.WriteLine("Auditing: " + new DirectoryInfo(folders[h]).Name);
-						parseManifestAndRun (folders[h], reportFile);
-					} catch (System.Xml.XmlException e) {
-						Console.WriteLine ("\tFailed to audit course " + new DirectoryInfo(folders [h]).Name);
-						Console.WriteLine ("\tInvalid XML found. Moving On.");
-						Console.WriteLine ("\tSource: " + e.Source);
-					} catch (Exception e) {
-						Console.WriteLine ("Failed to audit course " + new DirectoryInfo(folders [h]).Name);
-						Console.WriteLine ("\tSource: " + e.TargetSite);
-					}
-				}
-			}
-			Console.WriteLine ("---------- FINISHED AUDITING COURSES ----------");
-		}
+            // Run folders that contain a manifest  ---------------------------------------------------------
+            Console.WriteLine("---------- AUDITING COURSES          ---------- ");
+            String[] folders = Directory.GetDirectories(topDir);
+            for (int h = 0; h < folders.Length; h++)
+            {
+                if (File.Exists(Path.Combine(folders[h], "imsmanifest.xml")))
+                {
+                    try
+                    {
+                        Console.WriteLine("Auditing: " + new DirectoryInfo(folders[h]).Name);
+                        parseManifestAndRun(folders[h], reportFile);
+                    }
+                    catch (System.Xml.XmlException e)
+                    {
+                        Console.WriteLine("\tFailed to audit course " + new DirectoryInfo(folders[h]).Name);
+                        Console.WriteLine("\tInvalid XML found. Moving On.");
+                        Console.WriteLine("\tSource: " + e.Source);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Failed to audit course " + new DirectoryInfo(folders[h]).Name);
+                        Console.WriteLine("\tSource: " + e.TargetSite);
+                    }
+                }
+            }
+            Console.WriteLine("---------- FINISHED AUDITING COURSES ----------");
+        }
 		
-		// Given a course folder it read in the manifest, and the config to generate a line ------------------------------------------------------------
-		public static void parseManifestAndRun( String path, String reportfile) {
-			// Load Manifest file from specified path
-			XmlDocument manifest = new XmlDocument();
-			manifest.Load (path + "\\imsmanifest.xml");
+        // Given a course folder it read in the manifest, and the config to generate a line ------------------------------------------------------------
+        public static void parseManifestAndRun(String path, String reportfile)
+        {
+            // Load Manifest file from specified path
+            XmlDocument manifest = new XmlDocument();
+            manifest.Load(path + "\\imsmanifest.xml");
 
-			// Declare some variables
-			String doctitle, ident, filepath, orgunitid, stringBuilder = "", type;
+            // Declare some variables
+            String doctitle, ident, filepath, orgunitid, stringBuilder = "", type;
 
-			// Populate some variables
-			XmlNodeList resourceElems = manifest.GetElementsByTagName("resource");
-			XmlNodeList itemElems = manifest.GetElementsByTagName("item");
-			orgunitid = manifest.GetElementsByTagName ("manifest") [0].Attributes ["identifier"].InnerText.Substring (4);
+            // Populate some variables
+            XmlNodeList resourceElems = manifest.GetElementsByTagName("resource");
+            XmlNodeList itemElems = manifest.GetElementsByTagName("item");
+            orgunitid = manifest.GetElementsByTagName("manifest")[0].Attributes["identifier"].InnerText.Substring(4);
 
-			// Declare a CourseDocument
-			CourseDocument doc = new CourseDocument ();
+            // Declare a CourseDocument
+            CourseDocument doc = new CourseDocument();
 
-			XmlNodeList printItem = config.GetElementsByTagName ("printItem");
+            XmlNodeList printItem = config.GetElementsByTagName("printItem");
 
-			// Increment through the elements
-			for (int i = 0; i < resourceElems.Count; i++) {
-				if ("content".Equals (resourceElems [i].Attributes ["d2l_2p0:material_type"].InnerText)) { // If the item is a content page
-					filepath = resourceElems [i].Attributes ["href"].InnerText; // Get the path
-					for (int h = 0; h < itemElems.Count; h++) { // Increment through the items list
-						// If the item matches its resource item, and the document is infact an HTML pagem run!
-						if (itemElems [h].Attributes ["identifierref"].InnerText.Equals (resourceElems [i].Attributes ["identifier"].InnerText)) {
-							// Get the last set of data before running document
-							doctitle = itemElems [h].ChildNodes [0].InnerText;
-							ident = itemElems [h].Attributes ["identifier"].InnerText;
+            // Increment through the elements
+            for (int i = 0; i < resourceElems.Count; i++)
+            {
+                if ("content".Equals(resourceElems[i].Attributes["d2l_2p0:material_type"].InnerText))
+                { // If the item is a content page
+                    filepath = resourceElems[i].Attributes["href"].InnerText; // Get the path
+                    for (int h = 0; h < itemElems.Count; h++)
+                    { // Increment through the items list
+                        // If the item matches its resource item, and the document is infact an HTML pagem run!
+                        if (itemElems[h].Attributes["identifierref"].InnerText.Equals(resourceElems[i].Attributes["identifier"].InnerText))
+                        {
+                            // Get the last set of data before running document
+                            doctitle = itemElems[h].ChildNodes[0].InnerText;
+                            ident = itemElems[h].Attributes["identifier"].InnerText;
 
-							// Get rid of commas
-							doctitle = doctitle.Replace (",", "");
-							doctitle = doctitle.Replace ("\n", "");
+                            // Get rid of commas
+                            doctitle = doctitle.Replace(",", "");
+                            doctitle = doctitle.Replace("\n", "");
 
-							// If said HTML file exists, run
-							if (File.Exists (Path.Combine (path, filepath))) {
-								// Initialize the document
-								if (filepath.EndsWith ("html")) {
-									doc.loadDoc (Path.Combine (path, filepath), doctitle, orgunitid, ident);
+                            // If said HTML file exists, run
+                            if (File.Exists(Path.Combine(path, filepath)))
+                            {
+                                // Initialize the document
+                                if (filepath.EndsWith("html"))
+                                {
+                                    doc.loadDoc(Path.Combine(path, filepath), doctitle, orgunitid, ident);
 
-									// Start pulling data from the document and putting it into a return string
-									foreach (XmlNode node in printItem) {
-										type = node.SelectSingleNode ("query").Attributes ["type"].InnerText;
-										switch (type) {
-										case "OUI":
-											stringBuilder += orgunitid + ",";// OrgUnitID of document
-											break;
-										case "DocTitle":
-											stringBuilder += doctitle + ",";// Title as the course displays it
-											break;
-										case "htmlTitle":
-											stringBuilder += doc.getHtmlTitle () + ",";// HTML Title
-											break;
-										case "CSS":
-											stringBuilder += doc.CountQuery (getTextNode (node.SelectSingleNode ("query"))) + ","; // Gets a CSS Query
-											break;
-										case "RegEx":
-											stringBuilder += doc.CountRegEx (getTextNode (node.SelectSingleNode ("query"))) + ","; // Gets a RegEx
-											break;
-										case "Headers":
-											stringBuilder += doc.checkHeaders () + ","; // Checks Header
-											break;
-										case "Link":
-											stringBuilder += doc.generateD2lLink () + ","; // Generates a D2L Link
-											break;
-										}
-									}
-									PrintToFile (reportfile, stringBuilder + "\n");
-									stringBuilder = "";
-								}
-							}
-						}
-					}
-				}
-			}
-		}
+                                    // Start pulling data from the document and putting it into a return string
+                                    foreach (XmlNode node in printItem)
+                                    {
+                                        type = node.SelectSingleNode("query").Attributes["type"].InnerText;
+                                        switch (type)
+                                        {
+                                            case "OUI":
+                                                stringBuilder += orgunitid + ",";// OrgUnitID of document
+                                                break;
+                                            case "DocTitle":
+                                                stringBuilder += doctitle + ",";// Title as the course displays it
+                                                break;
+                                            case "htmlTitle":
+                                                stringBuilder += doc.getHtmlTitle() + ",";// HTML Title
+                                                break;
+                                            case "CSS":
+                                                stringBuilder += doc.CountQuery(getTextNode(node.SelectSingleNode("query"))) + ","; // Gets a CSS Query
+                                                break;
+                                            case "RegEx":
+                                                stringBuilder += doc.CountRegEx(getTextNode(node.SelectSingleNode("query"))) + ","; // Gets a RegEx
+                                                break;
+                                            case "Headers":
+                                                stringBuilder += doc.checkHeaders() + ","; // Checks Header
+                                                break;
+                                            case "Link":
+                                                stringBuilder += doc.generateD2lLink() + ","; // Generates a D2L Link
+                                                break;
+                                        }
+                                    }
+                                    PrintToFile(reportfile, stringBuilder + "\n");
+                                    stringBuilder = "";
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
 		
-		// Gets the text node from an element ----------------------------------------------------------------------------------------------------------
-		public static String getTextNode (XmlNode node) {
-			foreach (XmlNode child in node.ChildNodes)
-			{
-				if (child.NodeType == XmlNodeType.Text ||
-					child.NodeType == XmlNodeType.CDATA)
-				{
-					return child.Value;
-				}
-			} 
-			return "";
-		}
+        // Gets the text node from an element ----------------------------------------------------------------------------------------------------------
+        public static String getTextNode(XmlNode node)
+        {
+            foreach (XmlNode child in node.ChildNodes)
+            {
+                if (child.NodeType == XmlNodeType.Text ||
+                child.NodeType == XmlNodeType.CDATA)
+                {
+                    return child.Value;
+                }
+            } 
+            return "";
+        }
 
-		// Generates a header for the report from the config file --------------------------------------------------------------------------------------
-		public static String getHeader () {
-			XmlNodeList printItems = config.GetElementsByTagName ("printItem");
-			String stringBuilder = "";
-			foreach (XmlNode node in printItems) {
-				stringBuilder += getTextNode (node.SelectSingleNode ("printTitle")) + ",";
-			}
-			return stringBuilder + "\n";
-		}
+        // Generates a header for the report from the config file --------------------------------------------------------------------------------------
+        public static String getHeader()
+        {
+            XmlNodeList printItems = config.GetElementsByTagName("printItem");
+            String stringBuilder = "";
+            foreach (XmlNode node in printItems)
+            {
+                stringBuilder += getTextNode(node.SelectSingleNode("printTitle")) + ",";
+            }
+            return stringBuilder + "\n";
+        }
 		
-		// Appends a file with text --------------------------------------------------------------------------------------------------------------------
-		public static void PrintToFile(String file, String content) {
-			// Make the file if it doesn't exist
-			if (!File.Exists (file)) {
-				using (StreamWriter sw = File.CreateText (file)) {
-				}
-			}
+        // Appends a file with text --------------------------------------------------------------------------------------------------------------------
+        public static void PrintToFile(String file, String content)
+        {
+            // Make the file if it doesn't exist
+            if (!File.Exists(file))
+            {
+                using (StreamWriter sw = File.CreateText(file))
+                {
+                }
+            }
 
-			// Write to file if it does
-			using (StreamWriter sw = File.AppendText(file)) {
-				sw.Write (content);
-			}
-		}
+            // Write to file if it does
+            using (StreamWriter sw = File.AppendText(file))
+            {
+                sw.Write(content);
+            }
+        }
 		
-		// If a report of the same name is already there, delete it ------------------------------------------------------------------------------------
-		public static void startReportFile(String file) {
-			if (File.Exists (file)) {
-				File.Delete (file);
-			}
+        // If a report of the same name is already there, delete it ------------------------------------------------------------------------------------
+        public static void startReportFile(String file)
+        {
+            if (File.Exists(file))
+            {
+                File.Delete(file);
+            }
 
-		}
-	}
+        }
+    }
 }
--- CourseAuditor/CourseDocument.cs
+++ CourseAuditor/CourseDocument.cs
@@ -5,87 +5,107 @@ using CsQuery;
 
 namespace CourseAuditor
 {
-	public class CourseDocument
-	{
-		private HtmlAgilityPack.HtmlDocument htmlDoc;
-		private String DocName, OrgUnitID, DocID;
-		private CQ dom;
+    public class CourseDocument
+    {
+        private HtmlAgilityPack.HtmlDocument htmlDoc;
+        private String DocName, OrgUnitID, DocID;
+        private CQ dom;
 
-		public void loadDoc (String filepath, String docname, String orgunitid, String docid) {
-			// Load file into parser
-			htmlDoc = new HtmlAgilityPack.HtmlDocument();
-			htmlDoc.Load (filepath);
-			dom = CQ.Create (htmlDoc.DocumentNode.OuterHtml);
+        public void loadDoc(String filepath, String docname, String orgunitid, String docid)
+        {
+            // Load file into parser
+            htmlDoc = new HtmlAgilityPack.HtmlDocument();
+            htmlDoc.Load(filepath);
+            dom = CQ.Create(htmlDoc.DocumentNode.OuterHtml);
 
-			// Set variables
-			DocName = docname;
-			OrgUnitID = orgunitid;
-			DocID = docid;
-		}
+            // Set variables
+            DocName = docname;
+            OrgUnitID = orgunitid;
+            DocID = docid;
+        }
 
-		// Counts a CSS query
-		public int CountQuery (String query) {
-			CQ result = dom.Select (query);
-			return result.Length;
-		}
+        // Counts a CSS query
+        public int CountQuery(String query)
+        {
+            CQ result = dom.Select(query);
+            return result.Length;
+        }
 
-		// Counts occureneces of a Regular Expression
-		public int CountRegEx (String pattern) {
-			return Regex.Matches(htmlDoc.DocumentNode.OuterHtml, pattern).Count;
-		}
+        // Counts occureneces of a Regular Expression
+        public int CountRegEx(String pattern)
+        {
+            return Regex.Matches(htmlDoc.DocumentNode.OuterHtml, pattern).Count;
+        }
 
-		// Gets the HTML title
-		public String getHtmlTitle () {
-			String title, rawTitle = "";
-			if (dom.Select("title") != null) {
-				rawTitle = dom.Select("title").Html();
-			}
-			if (rawTitle != null && rawTitle != "") {
-				title = rawTitle;
-			} else {
-				title = "ERROR: NO TITLE FOUND";
-			}
-			title = title.Replace(",", "");
-			title = title.Replace ("\n", "");
-			return title;
-		}
+        // Gets the HTML title
+        public String getHtmlTitle()
+        {
+            String title, rawTitle = "";
+            if (dom.Select("title") != null)
+            {
+                rawTitle = dom.Select("title").Html();
+            }
+            if (rawTitle != null && rawTitle != "")
+            {
+                title = rawTitle;
+            }
+            else
+            {
+                title = "ERROR: NO TITLE FOUND";
+            }
+            title = title.Replace(",", "");
+            title = title.Replace("\n", "");
+            return title;
+        }
 
-		// Checks the headers for ADA compliance
-		public String checkHeaders () {
-			String headers = "";
-			if (dom.Select("h1").Length > 0) {
-				headers += "1";
-			}
-			if (dom.Select("h2").Length > 0) {
-				headers += "2";
-			}
-			if (dom.Select("h3").Length > 0) {
-				headers += "3";
-			}
-			if (dom.Select("h4").Length > 0) {
-				headers += "4";
-			}
-			if (dom.Select("h5").Length > 0) {
-				headers += "5";
-			}
-			if (dom.Select("h6").Length > 0) {
-				headers += "6";
-			}
-			if (headers == "") {
-				headers = "No Headers";
-			}
-			if ("123456".IndexOf (headers) == 0) {
-				return "Good: " + headers;
-			} else {
-				return "Bad: " + headers;
-			}
-		}
+        // Checks the headers for ADA compliance
+        public String checkHeaders()
+        {
+            String headers = "";
+            if (dom.Select("h1").Length > 0)
+            {
+                headers += "1";
+            }
+            if (dom.Select("h2").Length > 0)
+            {
+                headers += "2";
+            }
+            if (dom.Select("h3").Length > 0)
+            {
+                headers += "3";
+            }
+            if (dom.Select("h4").Length > 0)
+            {
+                headers += "4";
+            }
+            if (dom.Select("h5").Length > 0)
+            {
+                headers += "5";
+            }
+            if (dom.Select("h6").Length > 0)
+            {
+                headers += "6";
+            }
+            if (headers == "")
+            {
+                headers = "No Headers";
+            }
+            if ("123456".IndexOf(headers) == 0)
+            {
+                return "Good: " + headers;
+            }
+            else
+            {
+                return "Bad: " + headers;
+            }
+        }
 
-		// Generates a working link to the document
-		public String generateD2lLink () {
-			return "https://byui.brightspace.com/d2l/le/content/" + OrgUnitID + "/viewContent/" + DocID + "/View";
-		}
-	}
+        // Generates a working link to the document
+        public String generateD2lLink()
+        {
+            return "https://byui.brightspace.com/d2l/le/content/" + OrgUnitID + "/viewContent/" + DocID + "/View";
+        }
+    }
 
 
 }
